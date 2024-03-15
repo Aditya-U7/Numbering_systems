7d7437cd3f6da8a6bd7f7b0db3ced675899c876d
Aditya Upadhye <66748266+Aditya-U7@users.noreply.github.com>
2024-03-15 21:58:48 +0530

    Add files via upload

---
+++ binary_to_decimal.cpp
@@ -0,0 +1,54 @@
+/* 
+
+Author: Aditya Upadhye
+
+A simple program to convert binary to equivalent decimal. It works for upto 32-bit binary number.
+
+*/
+
+
+#include <iostream>
+#include <cmath>
+
+
+unsigned int binary_to_decimal(std::string n, int ln)
+{
+
+	ln = ln - 1;
+	unsigned int tc = ln;
+	unsigned int ansd = 0;
+
+	while (ln >= 0)
+	{
+
+		if (n[tc - ln] == '1')
+			ansd = ansd + 1 * std::pow(2, ln);
+
+		ln--;
+
+	} 
+
+	return ansd;
+}
+
+
+
+int main()
+{
+	std::string user_input;
+
+	std::cout << "Enter the valid binary number: " << std::endl;
+	std::cin >> user_input;
+
+	int length = user_input.length();
+
+	std::cout << "\nLength: " << length << std::endl;
+
+	if (length <= 32)
+		std::cout << "To Decimal: " << binary_to_decimal(user_input, length) << "\n\n";
+	else
+		std::cout << "Length exceeds the 32 bit allowed range." << std::endl;
+
+	return 0;
+
+}
---
+++ binary_to_octal.cpp
@@ -0,0 +1,102 @@
+/*
+
+Author: Aditya Upadhye
+
+A program to convert a valid binary number given as input to an equivalent octal number using a map.
+
+*/
+
+#include <iostream>
+#include <map>
+
+
+void binary_to_octal(std::string n)
+{
+
+	std::map<std::string, std::string> bin_to_oct {{"000","0"}, {"001","1"}, {"010","2"}, {"011","3"}, {"100","4"}, {"101","5"}, {"110","6"}, {"111","7"}};
+
+
+	std::string ans = "";
+
+	int no_of_calls = (n.length() / 3) + (n.length() % 3 == 0? 0 : 1);
+
+	int start_index = 0;
+
+	int remainder = n.length() % 3;
+
+	short count = 0;
+
+	int end_index = 3;
+
+	while (no_of_calls)
+	{
+
+		std::string tmp = "";
+
+		end_index = 3;
+
+		if (count == 0)
+		{       
+			if (remainder == 1)
+			{
+				tmp = "00";
+				end_index = 1;
+			}
+			else if (remainder == 2)
+			{
+				tmp = "0";
+				end_index = 2;
+			}
+		}
+
+
+
+		tmp = tmp + n.substr(start_index, end_index);
+		ans = ans + bin_to_oct[tmp];
+
+		if (count == 0)
+		{
+			if (remainder == 1)
+			{
+				start_index += 1;
+			}
+			else if (remainder == 2)
+			{
+				start_index += 2;
+			}
+			else
+			{
+				start_index += 3;
+			}
+			count++;
+
+		}
+
+		else
+		{
+			start_index += 3;
+		}
+
+		no_of_calls--;
+	        
+	}
+
+
+	std::cout << "\n\nBinary to octal:\n" << ans << std::endl;
+
+}
+
+
+int main()
+{
+
+	std::cout << "Enter the binary number: " << std::endl;
+	std::string user_input;
+	std::cin >> user_input;
+
+
+	binary_to_octal(user_input);
+
+	return 0;
+
+}
